51d044af896686256f3ab9ba66dfd8d74f3a8a63
adamatusz <adamatusz@gmail.com>
2024-10-23 19:56:38 +0200

    Zadanie 1

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,5 +1,17 @@
 const App = () => {
-  return <div><i>Jest git</i></div>;
+  return (
+    <>  
+    <div>
+    <h1 className="header">Witaj w React</h1>
+    <p>To jest paragraf w React</p>
+    <div>
+    <span>React jest </span>
+    <span> niesamowity.</span>
+    </div> 
+    </div>
+    <footer>Stopka</footer>
+    </>
+)
 };
 
 export default App;
