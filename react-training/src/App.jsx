11f63e2df8e5010885fc3d1cbd93f45342617c77
adamatusz <adamatusz@gmail.com>
2024-10-24 14:20:58 +0200

    execises of lesson

--- react-training/src/App.jsx
+++ react-training/src/App.jsx
@@ -1,15 +1,37 @@
+// const App = () => {
+
+// const header = "Witaj w React"
+
+//   return (
+//     <>
+//       <div>
+//         <h1 className="header">{header}</h1>
+//         <p>To jest paragraf w React</p>
+//         <div>
+//           <span>React jest </span>
+//           <span> niesamowity.</span>
+//         </div>
+//       </div>
+//       <footer>Stopka</footer>
+//     </>
+//   );
+/* 
+  Twoim zadaniem jest modyfikacja istniejącego kodu w taki sposób,
+  aby oprócz godzin i minut wyświetlał również sekundy. Czas powinien
+  być wyświetlany w formacie hh:mm:ss, np. 01:05:07. 
+  Pamiętaj, aby dodać wiodące zera, jeśli godziny, minuty lub sekundy są jednocyfrowe.
+*/
+const currentDate = new Date();
+
 const App = () => {
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getMinutes()).padStart(2, "0");
   return (
     <>
-      <div>
-        <h1 className="header">Witaj w React</h1>
-        <p>To jest paragraf w React</p>
-        <div>
-          <span>React jest </span>
-          <span> niesamowity.</span>
-        </div>
-      </div>
-      <footer>Stopka</footer>
+      <h1>
+        {hours}:{minutes}:{seconds}
+      </h1>
     </>
   );
 };
